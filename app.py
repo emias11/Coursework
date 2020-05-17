7ec63b1d56afb7f18467a1fe9d5f0ce8f8c9913f
Finn Snape <finnsnape@gmail.com>
2020-05-18 00:44:44 +0100

    ui

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import render_template, Flask, jsonify, redirect, request, url_for
+import bs
+import bs2
+
+app = Flask(__name__)
+results = []
+
+
+@app.route("/", methods=["GET", "POST"])
+def load_app():
+	global results
+	# maybe better to have subdictionaries like {"name": "Money Money Money", "url": "..."}
+	if request.method == "GET":
+		return render_template("index.html")
+	else:
+		query = request.form["query"]
+		result1 = bs.main(query)
+		result2 = bs2.main(query)
+		if result1 and not result2:
+			results = result1
+		elif result2 and not result1:
+			results = result2
+		elif result1 and result2:
+			results = result1 + result2
+		else:
+			return render_template("index.html")
+		search_results = {}
+		for i in range(len(results) - 1):
+			search_results[i] = results[i]
+		amount = len(search_results)
+	return render_template("index.html", search_results=search_results, amount=amount)
+
+
+@app.route("/retrieve_instruments", methods=["POST"])
+def retrieve_instruments():
+	global results
+	data = request.json
+	songs = data["songs"]
+	instruments = ["Guitar", "Piano"]
+	print(songs)
+	for song in songs:
+		print(results[song])
+	# download songs. we have the download links stored in results global variable
+	# only download if not already downloaded!
+	# extract instruments from new downloaded as well as already downloaded
+	# return in this function
+	return jsonify({"instruments": instruments})
+
+
+@app.route("/process", methods=["POST"])
+def process():
+	if request.method == "POST":
+		data = request.form  # this will be the songs selected and the instruments selected
+		songs = [int(i) for i in data["songs"].split(",")]
+		instruments = data["instruments"].split(",")
+		print(songs)
+		print(instruments)
+	# do some tings with it
+	return render_template("process.html")
+
+
+def main():
+	app.run(debug=True)
+
+
+if __name__ == "__main__":
+	main()
--- bs2.py
+++ bs2.py
@@ -5,48 +5,42 @@ import time
 
 
 def scrape_results(search_query):
-    html = requests.get(f"https://bitmidi.com/search/?q={search_query}").text
-    json_data = get_json(html)
-    page_total = json_data["views"]["search"][search_query]["pageTotal"]
-    midi_with_names = get_midi_from_json(json_data)
-    for i in range(1, page_total):
-        html = requests.get(f"https://bitmidi.com/search/?q={search_query}&page={i}").text
-        json_data = get_json(html)
-        midi_with_names += get_midi_from_json(json_data)
-    return midi_with_names
+	html = requests.get(f"https://bitmidi.com/search/?q={search_query}").text
+	json_data = get_json(html)
+	page_total = json_data["views"]["search"][search_query]["pageTotal"]
+	midi_with_names = get_midi_from_json(json_data)
+	for i in range(1, page_total):
+		html = requests.get(f"https://bitmidi.com/search/?q={search_query}&page={i}").text
+		json_data = get_json(html)
+		midi_with_names += get_midi_from_json(json_data)
+	return midi_with_names
 
 
 def get_midi_from_json(json_data):
-    all_midis = json_data["data"]["midis"]
-    midi_with_names = [[all_midis[key]["id"], all_midis[key]["name"]] for key in all_midis]
-    return midi_with_names
+	all_midis = json_data["data"]["midis"]
+	midi_with_names = [[all_midis[key]["name"], "https://bitmidi.com/uploads/" + str(all_midis[key]["id"])] for key in all_midis]
+	return midi_with_names
 
 
 def get_json(html):
-    soup = BeautifulSoup(html, "html.parser")
-    data = soup.find("script").text.replace("\n", "").split("console.time('render')window.initStore =")[1]
-    json_data = json.loads(data)
-    return json_data
+	soup = BeautifulSoup(html, "html.parser")
+	data = soup.find_all("script")[1].text.replace("\n", "").split("console.time('render')window.initStore =")[1]
+	json_data = json.loads(data)
+	return json_data
 
 
 def save_midi(id, name):
-    song = requests.get(f"https://bitmidi.com/uploads/{id}.mid")
-    open(f"{name}.mid", "wb").write(song.content)
+	song = requests.get(f"https://bitmidi.com/uploads/{id}.mid")
+	open(f"{name}.mid", "wb").write(song.content)
 
 
-def main():
-    search = input("what search_query")
-    start = time.time()
-    lst = scrape_results(search)
-    print(lst)
-    # for item in lst:
-       # save_midi(item[0], item[1])
-    end = time.time()
-    print(end-start)
+def main(query):
+	results = scrape_results(query)
+	return results
 
 
 if __name__ == '__main__':
-    main()
+	main("")
 
 #     if you want to speed that up it's not that hard just get the number of pages for the search query
 #     with json_data["views"]["search"]["mozart"]["pageTotal"] then only run it that number of times
