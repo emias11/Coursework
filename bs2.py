4fcdcf73b2dc6dc28ebd955e896d3d98964fef7d
Finn Snape <finnsnape@gmail.com>
2020-05-18 02:51:43 +0100

    pretty ui

--- bs2.py
+++ bs2.py
@@ -19,7 +19,7 @@ def scrape_results(search_query):
 
 def get_midi_from_json(json_data):
 	all_midis = json_data["data"]["midis"]
-	midi_with_names = [{"name": all_midis[key]["name"], "url": "https://bitmidi.com/uploads/" + str(all_midis[key]["id"])} for key in all_midis]
+	midi_with_names = [{"name": all_midis[key]["name"], "url": "https://bitmidi.com/uploads/" + str(all_midis[key]["id"]) + ".mid"} for key in all_midis]
 	return midi_with_names
 
 
